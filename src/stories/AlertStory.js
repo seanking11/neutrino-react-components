de88f4046d1ede43b8e1b0aa0a93140c6987bbf5
Sean King <kingsc@miamioh.edu>
2017-06-20 10:15:34 -0500

    feat(AlertStory): Add AlertStory to storybook.js

---
+++ src/stories/AlertStory.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { storiesOf, addDecorator, setAddon } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import { WithNotes } from '@storybook/addon-notes'
+import { withKnobs, text, boolean, number, select, object, array } from '@storybook/addon-knobs'
+import JSXAddon from 'storybook-addon-jsx'
+import Alert from '../components/Alert'
+
+setAddon(JSXAddon)
+
+storiesOf('Alert', module)
+  .addDecorator(withKnobs)
+  .addWithJSX('Default Alert', () => {
+    const options = [
+      Alert.types.DANGER,
+      Alert.types.SUCCESS,
+      Alert.types.INFO,
+      Alert.types.WARNING
+    ]
+
+    const styles = {
+      'padding': '1em 1.5em'
+    }
+
+    return (
+      <div>
+        <Alert
+          show={boolean('Show?', true)}
+          title={text('Title', 'Dabnabit!')}
+          type={select('Type', options, Alert.types.DANGER)}
+          onClose={() => console.log('Nope.')}
+          styles={object('Styles', styles)}
+          largeAlert={boolean('Large?', false)}
+        >
+          {text('Message', 'Shit is florida')}
+        </Alert>
+      </div>
+    )
+  })
